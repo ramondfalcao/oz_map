691188263a8529a2eb9858fe81bb139b6a5205d6
ramond_falcao <ramondfalcao@gmail.com>
2025-05-18 22:24:52 -0300

    fix: fix import express async errors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json';
 import regionRoutes from './routes/region.routes';
