12aa8b1c9f37fd67e9444882d9da8aba865a4115
ramond_falcao <ramondfalcao@gmail.com>
2025-05-18 22:04:48 -0300

    feat: Create Validate Body middlewarer

---
+++ src/middlewares/validateBody.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
+
+export const validateBody = (schema: Joi.ObjectSchema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error, value } = schema.validate(req.body, {
+      abortEarly: false,
+      allowUnknown: false,
+      stripUnknown: true,
+    });
+
+    if (error) {
+      return res.status(400).json({
+        error: 'Data validation failed',
+        details: error.details.map((detail) => detail.message),
+      });
+    }
+
+    req.body = value;
+    next();
+  };
+};
