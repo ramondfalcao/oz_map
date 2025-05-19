70db9e3af04d5b982e0a65c73db6f9c6d2e140f0
ramond_falcao <ramondfalcao@gmail.com>
2025-05-18 22:09:27 -0300

    feat: Init CRUD routes in app

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json';
-import routes from './routes';
+import regionRoutes from './routes/region.routes';
+import { connectDB } from './config/database';
+import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api', routes);
+connectDB();
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use('/api/regions', regionRoutes);
+
+app.use(errorHandler);
 
 export default app;
