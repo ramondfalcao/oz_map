f8f8288a51ee23608528da819a6e672b70bdd391
ramond_falcao <ramondfalcao@gmail.com>
2025-05-18 22:08:31 -0300

    feat: Create class app error

---
+++ src/utils/AppError.ts
@@ -0,0 +1,10 @@
+export class AppError extends Error {
+  constructor(
+    public message: string,
+    public statusCode: number = 400,
+    public details?: unknown
+  ) {
+    super(message);
+    this.name = 'AppError';
+  }
+}
