fd7c557117bb4b638925c3f18b7a8352211de6d7
ramond_falcao <ramondfalcao@gmail.com>
2025-05-20 02:37:01 -0300

    remove logs

--- src/controllers/geolocation.controller.ts
+++ src/controllers/geolocation.controller.ts
@@ -6,15 +6,13 @@ import { AppError } from '../utils/AppError';
 export const getRegionsByAddress = async (req: Request, res: Response) => {
   try {
     const address = req.query.address as string;
-    console.log(address);
-    
+
     if (!address) {
       throw new AppError('address parameter is mandatory', 400)
     }
 
     const { lat, lng } = await geocodeAddress(address);
     const regions = await findRegionsContainingPoint(lat, lng);
-    console.log(regions);
 
     res.json(regions);
   } catch (err: any) {
